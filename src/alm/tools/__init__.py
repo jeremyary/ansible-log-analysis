3833d68d64a7caa20d6ad7a06dcdeeb53836381d
Yossi Elias <yelias@redhat.com>
2026-01-05 15:30:41 +0200

    fix timstamp issue:
    - sort by database_timestamp not real_timestamp
    - LogEntry.timestamp need to be real_timestamp
    - suport ISO format for LogEntry.timestamp converter
    - add converter for LogLabels.database_timestamp
    
    Signed-off-by: Yossi Elias <yelias@redhat.com>

--- src/alm/tools/__init__.py
+++ src/alm/tools/__init__.py
@@ -8,10 +8,12 @@ from alm.tools.loki_tools import (
     create_log_lines_above_tool,
     LOKI_STATIC_TOOLS,
 )
+from alm.tools.loki_helpers import timestamp_to_utc_datetime
 
 __all__ = [
     "get_logs_by_file_name",
     "search_logs_by_text",
     "create_log_lines_above_tool",
+    "timestamp_to_utc_datetime",
     "LOKI_STATIC_TOOLS",
 ]
